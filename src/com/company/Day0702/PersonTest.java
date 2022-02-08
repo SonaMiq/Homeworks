de4c292c90bba04213cd1552fd888ffe7cc63780
Sona <sonmiq@gmail.com>
2022-02-08 11:32:38 +0400

    Homework 0702 changes

--- src/com/company/Day0702/PersonTest.java
+++ src/com/company/Day0702/PersonTest.java
@@ -2,9 +2,11 @@ package com.company.Day0702;
 
 public class PersonTest {
     public static void main(String[] args) {
+        
         Person prs1 = new Person("Sona", 34, "Yerevan,Davtashen 4 4-44");
         Person prs2 = new Person("Anna", 30, "Yerevan,Davtashen 2 1-88");
         prs1.printInfo();
         prs2.printInfo();
     }
 }
+
